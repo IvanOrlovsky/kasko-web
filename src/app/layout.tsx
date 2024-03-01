5c0e48f21c56f4857b8947e53bc4bb4dcf0b9505
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-01 21:49:37 +0000

    added header to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import "./globals.css";
+import Header from "@/components/Header";
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ru">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="ru">
+			<Header />
+			<body className="bg-[#F1F2F6]">{children}</body>
+		</html>
+	);
 }
