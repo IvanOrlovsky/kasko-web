337254937d7b93341fe11fd366639c3fc8c5a7fd
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-01 22:01:17 +0000

    fixed hiding content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import MainLogo from "./logo/MainLogo";
 
 export default function Header() {
 	return (
-		<header className="flex-no-wrap fixed top-0 flex w-full bg-[#ffffff] px-5 py-5">
+		<header className="flex-no-wrap sticky top-0 flex w-full bg-[#ffffff] px-5 py-5 z-10">
 			<MainLogo />
 		</header>
 	);
