d193a1bf7e3183dbb2d0ba0a0211d085658c8c01
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-01 22:44:21 +0000

    Made OK Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
 import MainLogo from "./logo/MainLogo";
+import FeedbackOutlinedIcon from "@mui/icons-material/FeedbackOutlined";
+import PhoneOutlinedIcon from "@mui/icons-material/PhoneOutlined";
 
 export default function Header() {
 	return (
-		<header className="flex-no-wrap sticky top-0 flex w-full bg-[#ffffff] px-5 py-5 z-10">
-			<MainLogo />
+		<header className="flex-no-wrap justify-between sticky top-0 flex w-full bg-[#ffffff] px-5 py-5 z-10">
+			<div className=" ">
+				<MainLogo />
+			</div>
+
+			<div className="flex flex-row gap-2">
+				<div className="rounded-full bg-[#F1F2F6] p-2">
+					<FeedbackOutlinedIcon />
+				</div>
+				<div className="rounded-full bg-[#F1F2F6] p-2">
+					<PhoneOutlinedIcon />
+				</div>
+			</div>
 		</header>
 	);
 }
