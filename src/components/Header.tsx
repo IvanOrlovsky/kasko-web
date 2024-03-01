cf720deeb9f40ffdec6e646116d7d3d2581e0770
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-01 21:50:21 +0000

    Started making Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import MainLogo from "./logo/MainLogo";
+
+export default function Header() {
+	return (
+		<header className="flex-no-wrap fixed top-0 flex w-full bg-[#ffffff] px-5 py-5">
+			<MainLogo />
+		</header>
+	);
+}
