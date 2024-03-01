be6ce51023fcfb0d6d1a4a868dba6a15d355c015
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-01 20:49:00 +0000

    Created NavChip component

---
+++ src/components/data-display/NavChip.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Avatar from "@mui/material/Avatar";
+import Chip from "@mui/material/Chip";
+import { useState } from "react";
+
+type NavChipProps = {
+	letterInCircle: string;
+	label: string;
+	active: boolean;
+	onClick?: () => void;
+
+	disabled?: boolean;
+};
+
+export default function NavChip({
+	letterInCircle,
+	label,
+	onClick,
+	disabled,
+	active,
+}: NavChipProps) {
+	const handleClick = () => {
+		if (onClick) {
+			onClick();
+		}
+	};
+
+	return (
+		<Chip
+			disabled={disabled}
+			onClick={handleClick}
+			className={
+				!active
+					? "bg-transparent hover:bg-transparent"
+					: "bg-[#C5EAFC] hover:bg-[#C5EAFC]"
+			}
+			avatar={
+				<Avatar sx={{ backgroundColor: "white" }}>
+					{letterInCircle}
+				</Avatar>
+			}
+			label={label}
+		/>
+	);
+}
