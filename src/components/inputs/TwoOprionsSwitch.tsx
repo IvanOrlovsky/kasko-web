e52d2a8400ae27b24e336b7dbc2852ddd53815be
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-02 22:24:02 +0300

    done with switcher without registration

---
+++ src/components/inputs/TwoOprionsSwitch.tsx
@@ -0,0 +1,37 @@
+import { useFormContext } from "react-hook-form";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+type TwoOptionsSwitchProps = {
+	name: string;
+	option1: string;
+	option2: string;
+};
+
+export default function TwoOptionsSwitch({
+	name,
+	option1,
+	option2,
+}: TwoOptionsSwitchProps) {
+	const { register, setValue, watch } = useFormContext();
+	const selectedOption = watch(name); // Получаем текущее значение из react-hook-form
+
+	const handleTabChange = (value: string) => {
+		setValue(name, value);
+	};
+
+	return (
+		<>
+			<input
+				type="text"
+				{...register(name)}
+				value={selectedOption || ""}
+			/>
+			<Tabs defaultValue={option1} onChange={handleTabChange}>
+				<TabsList>
+					<TabsTrigger value={option1}>{option1}</TabsTrigger>
+					<TabsTrigger value={option2}>{option2}</TabsTrigger>
+				</TabsList>
+			</Tabs>
+		</>
+	);
+}
