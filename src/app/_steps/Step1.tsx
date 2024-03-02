6f978b0a6654f988a88ab5cde86b4fc779985270
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-02 14:43:20 +0300

    started making step1

--- src/app/_steps/Step1.tsx
+++ src/app/_steps/Step1.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import FormBlock from "@/components/FormBlock";
-import SimpleInput from "@/components/inputs/SimpleInput";
-import CheckBox from "@/components/inputs/CheckBox";
 import { useForm, FormProvider } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 import { useMainContext } from "@/contexts/MainContext";
+import Auto from "./Step1Blocks/Auto";
 
 type FormValues = {
 	isCarRegistered: boolean;
@@ -53,7 +52,7 @@ export default function Step1() {
 		},
 	});
 
-	const { register, handleSubmit } = form;
+	const { register, handleSubmit, trigger, watch } = form;
 
 	const { setStep } = useMainContext();
 
@@ -62,42 +61,9 @@ export default function Step1() {
 	return (
 		<FormProvider {...form}>
 			<form>
-				<FormBlock title="Автомобиль">
-					<>
-						<CheckBox
-							name="isCarRegistered"
-							label="Авто еще не зарегистрировано"
-							hint="Отметьте галочкой, если авто не зарегистрировано, чтобы перейти к заполнению данных об авто."
-						/>
-						{/* /^[АВЕКМНОРСТУХ]\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui */}
-						<SimpleInput
-							name="VIN"
-							placeholder="A 000 AA 00"
-							pattern={
-								/^[АВЕКМНОРСТУХ]\s?\d{3}\s?[АВЕКМНОРСТУХ]{2}\s?\d{2,3}$/iu
-							}
-							patternMsg="Введенное значение не удволетворяет является госномером"
-							helper="Введите госномер автомобиля чтобы мы нашли данные о нем"
-						/>
-						<button
-							type="button"
-							className="bg-[#1698D9] hover:bg-[#0e81bb] active:bg-[#0a5880] rounded-2xl mb-5 py-[15px] font-roboto text-white font-semibold text-lg"
-						>
-							Найти
-						</button>
-						<CheckBox
-							name="hasActiveKasko"
-							label="Есть действующий полис КАСКО"
-							hint="Отметьте галочкой, если есть действующий полис КАСКО."
-						/>
-						<CheckBox
-							name="isCarInCredit"
-							label="Автомобиль в кредите"
-							hint="Отметьте галочкой, если Автомобиль в кредите."
-						/>
-					</>
-				</FormBlock>
+				<Auto />
 			</form>
+			<DevTool control={form.control} />
 		</FormProvider>
 	);
 }
