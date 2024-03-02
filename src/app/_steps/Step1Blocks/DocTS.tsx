9bcc1a11957fa5087e5bc60630c10ef01ef9dee4
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-02 17:59:14 +0300

    opt render

--- src/app/_steps/Step1Blocks/DocTS.tsx
+++ src/app/_steps/Step1Blocks/DocTS.tsx
@@ -4,45 +4,47 @@ import SimpleSelect from "@/components/inputs/SimpleSelect";
 import { useFormContext } from "react-hook-form";
 
 export default function DocTS() {
-	const { watch, trigger } = useFormContext();
+	const { watch } = useFormContext();
 
-	return (
-		<FormBlock title="Документ ТС">
-			<div className="flex flex-col gap-4">
-				<SimpleSelect
-					data={[
-						"ПТС",
-						"ЭПТС",
-						"СТС",
-						"ТСП",
-						"АТС",
-						"ИТС",
-						"МТС",
-						"УТС",
-						"КТС",
-						"ВТС",
-						"ЛТС",
-					]}
-					name="TSdocument"
-					label="Документ ТС"
-					required={watch("isCarRegistered")}
-					requiredMsg="Вы не ввели документ ТС!"
-				></SimpleSelect>
-				<SimpleInput
-					name="todayCost"
-					label="Текущая рыночная стоимость"
-					pattern={/^\d+$/}
-					patternMsg="Вы ввели не число!"
-					required={!watch("isCarRegistered")}
-					requiredMsg="Необходимо ввести текущую рыночную стоимость."
-				/>
-				<SimpleInput
-					name="VIN"
-					label="VIN"
-					required={!watch("isCarRegistered")}
-					requiredMsg="Необходимо ввести VIN номер."
-				/>
-			</div>
-		</FormBlock>
-	);
+	if (watch("isCarRegistered")) {
+		return (
+			<FormBlock title="Документ ТС">
+				<div className="flex flex-col gap-4">
+					<SimpleSelect
+						data={[
+							"ПТС",
+							"ЭПТС",
+							"СТС",
+							"ТСП",
+							"АТС",
+							"ИТС",
+							"МТС",
+							"УТС",
+							"КТС",
+							"ВТС",
+							"ЛТС",
+						]}
+						name="TSdocument"
+						label="Документ ТС"
+						required={watch("isCarRegistered")}
+						requiredMsg="Вы не ввели документ ТС!"
+					></SimpleSelect>
+					<SimpleInput
+						name="todayCost"
+						label="Текущая рыночная стоимость"
+						pattern={/^\d+$/}
+						patternMsg="Вы ввели не число!"
+						required={!watch("isCarRegistered")}
+						requiredMsg="Необходимо ввести текущую рыночную стоимость."
+					/>
+					<SimpleInput
+						name="VIN"
+						label="VIN"
+						required={!watch("isCarRegistered")}
+						requiredMsg="Необходимо ввести VIN номер."
+					/>
+				</div>
+			</FormBlock>
+		);
+	}
 }
