86458fb1acd903d396e7ec1bfc50e6936c61a938
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-02 19:47:13 +0300

    made footer for further steps

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+	return (
+		<div className="bg-white rounded-t-2xl p-4 flex flex-col shrink-0 fixed bottom-0 min-w-full">
+			<h1 className="font-roboto text-xs text-[#737B8C] ">
+				Цена подписки
+			</h1>
+			<span className="text-[#1F232B] font-roboto font-semibold text-2xl">
+				XXXX ₽
+			</span>
+			<span className="text-[#1F232B] font-roboto font-normal text-xs">
+				в месяц
+			</span>
+		</div>
+	);
+}
