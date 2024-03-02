72bb777992d0eba8116e03320339525438f22983
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-02 17:54:56 +0300

    i hope done with simpleinput

--- src/components/inputs/SimpleInput.tsx
+++ src/components/inputs/SimpleInput.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { TextField } from "@mui/material";
-import { useFormContext, Controller } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 import { styled } from "@mui/material";
-import InputMask from "react-input-mask";
 
 const StyledSimpleInput = styled(TextField)({
-	height: "3em",
 	"& .MuiTextField-root": {
 		backgroundColor: "#F1F2F6",
 		borderRadius: "4px",
@@ -21,7 +19,7 @@ type SimpleInputProps = {
 	requiredMsg?: string;
 	helper?: string;
 	placeholder?: string;
-	mask?: string;
+	pattern?: RegExp;
 	patternMsg?: string;
 };
 
@@ -32,54 +30,34 @@ export default function SimpleInput({
 	helper,
 	requiredMsg,
 	placeholder,
-	mask,
+	pattern,
+	patternMsg,
 }: SimpleInputProps) {
 	const {
-		control,
+		register,
 		formState: { errors },
 		clearErrors,
 	} = useFormContext();
 
 	return (
-		<Controller
-			control={control}
-			name={name}
-			rules={{
-				required: required
-					? requiredMsg || "Данное поле обязательно"
-					: false,
-			}}
-			render={({ field }) => (
-				<InputMask
-					mask={mask}
-					maskChar=" "
-					value={field.value}
-					onChange={field.onChange}
-				>
-					{(inputProps: any) => (
-						<StyledSimpleInput
-							{...inputProps}
-							helperText={
-								errors[name]?.message
-									? String(errors[name]?.message)
-									: helper
-							}
-							placeholder={placeholder}
-							label={label}
-							required={required}
-							fullWidth={true}
-							variant="filled"
-							onChange={(e) => {
-								field.onChange(e);
-								clearErrors(name);
-							}}
-							value={field.value}
-							error={!!errors[name]?.message}
-							className="mb-12"
-						/>
-					)}
-				</InputMask>
-			)}
-		/>
+		<StyledSimpleInput
+			helperText={
+				errors[name]?.message
+					? (errors[name]?.message as string)
+					: helper
+			}
+			placeholder={placeholder}
+			fullWidth
+			label={label}
+			variant="filled"
+			{...register(name, {
+				onChange: () => clearErrors(name),
+				required: required ? requiredMsg : false,
+				pattern: pattern
+					? { value: pattern, message: patternMsg as string }
+					: undefined,
+			})}
+			error={!!errors[name]?.message}
+		></StyledSimpleInput>
 	);
 }
