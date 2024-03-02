8efcd79652e753e8a65a7882e3fdcbf5c8c0e306
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-02 22:24:25 +0300

    added datepicker

--- src/app/_steps/Step2Blocks/InsuranceParams.tsx
+++ src/app/_steps/Step2Blocks/InsuranceParams.tsx
@@ -5,6 +5,8 @@ import RangeInput from "@/components/inputs/RangeInput";
 import AutoComplete from "@/components/inputs/AutoComplete";
 import Regions from "../../../../public/datasets/Regions.json";
 import { useFormContext } from "react-hook-form";
+import TwoOptionsSwitch from "@/components/inputs/TwoOprionsSwitch";
+import DateInput from "@/components/inputs/DataInput";
 
 export default function InsuranceParams() {
 	return (
@@ -15,6 +17,42 @@ export default function InsuranceParams() {
 					title="Регион использования ТС"
 					dataset={Regions}
 				/>
+
+				<div className="w-full flex flex-col items-center my-5">
+					<h2 className="self-start font-roboto text-[#1F232B] text-[16px] font-bold ">
+						Ремонт от страховщика
+					</h2>
+					<TwoOptionsSwitch
+						name="repair"
+						option1="Дилерская СТОА"
+						option2="Обычная СТОА"
+					/>
+					<span className="self-start font-roboto text-[#737B8C] text-[14px] font-medium">
+						СТОА официального дилера Subaru
+					</span>
+				</div>
+				<div className="w-full flex flex-col items-center my-5">
+					<RangeInput
+						title="Франшиза"
+						name="franchaise"
+						isCurrency={true}
+						min={0}
+						max={30000}
+						step={5000}
+						marks={[
+							{ value: 0, label: "нет" },
+							{ value: 5000 },
+							{ value: 10000 },
+							{ value: 15000 },
+							{ value: 20000 },
+							{ value: 25000 },
+							{ value: 30000 },
+						]}
+					/>
+				</div>
+				<div className="w-full flex flex-col items-center my-5">
+					<DateInput />
+				</div>
 			</>
 		</FormBlock>
 	);
