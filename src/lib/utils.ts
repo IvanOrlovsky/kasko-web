d58657dc683d8f2ca1e7c59bbb65dc0e63d22aae
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-03-05 20:45:58 +0300

    added empty fields check func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,32 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function hasEmptyFields(
+	obj: {
+		GOSnumber: string;
+		make: string;
+		model: string;
+		releaseYear: string;
+		bodyType: string;
+		gearBoxType: string;
+		engine: string;
+		modification: string;
+		TSdocument: string;
+		PTSnumber: string;
+		VIN: string;
+	},
+	excludedFields: string[]
+) {
+	for (const key in obj) {
+		if (Object.prototype.hasOwnProperty.call(obj, key)) {
+			if (!excludedFields.includes(key) && !obj[key]) {
+				return true;
+			}
+		}
+	}
+	return false;
 }
